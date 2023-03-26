3a1ca3041e3d79a21e2713aabace141a23b7e375
«DenonG <Gorlachuk-d-v@yandex.ru>
2023-03-26 19:44:22 +0300

    Сделал задачу №8 в Task8

---
+++ Lesson1/Task8/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+Console.WriteLine("Введите число, а я его разложу на четные");
+string strNum1 = Console.ReadLine();
+int num1 = Convert.ToInt32(strNum1);
+int count = 1;
+while (count < num1 + 1){
+    if (count%2 == 0){
+        Console.Write($"{count}, ");
+    }
+    count+=1;
+}
